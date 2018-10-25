7468ac196c5d010c03ad13c5d1d2044e776c8287
Sondetsu <sondetsu@gmail.com>
2018-10-25 11:37:08 -0500

    Added the about controller and the early prototype of the nav bar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'rebels/index'
-  get 'rebels/show'
+  get 'about/index'
+  resources :rebels, only: [:index]
+
+  #get 'rebels/index'
+  #get 'rebels/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'rebels#index'
 end
