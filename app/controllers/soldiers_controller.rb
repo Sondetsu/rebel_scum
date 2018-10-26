fc0d1272a35fafb35232908c7a55689eb81d9a8b
Sondetsu <sondetsu@gmail.com>
2018-10-26 00:21:40 -0500

    Fully populated the all results view.

--- app/controllers/soldiers_controller.rb
+++ app/controllers/soldiers_controller.rb
@@ -1,7 +1,9 @@
 class SoldiersController < ApplicationController
   def index
+    @soldiers = Soldier.all
   end
 
   def show
+    @soldier = Soldier.find(params[:id])
   end
 end
--- app/controllers/squadrons_controller.rb
+++ app/controllers/squadrons_controller.rb
@@ -1,7 +1,10 @@
 class SquadronsController < ApplicationController
   def index
+    @squadrons = Squadron.all
   end
 
   def show
+    @squadron = Squadron.find(params[:id])
+    @soldiers = squadron.soldier
   end
 end
